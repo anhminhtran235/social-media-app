4e59fe238ec8f6b09be13838fe55af56e74a3a5e
Anh Minh Tran <anhminhtran235@gmail.com>
2020-10-20 16:07:43 -0700

    minor

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
+    console.log('Hit *');
+    console.log(req);
+    console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
