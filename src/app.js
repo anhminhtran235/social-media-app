e1aac94595fdba525ae21b3d3b31831d68edaf7a
Anh Minh Tran <anhminhtran235@gmail.com>
2020-10-20 10:56:24 -0700

    minor fix

--- src/app.js
+++ src/app.js
@@ -25,7 +25,6 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('../client/build'));
 
-  // Serve file
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
