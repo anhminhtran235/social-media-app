15c872ff9d88a202f50e0c5520847df487007a8b
Anh Minh Tran <anhminhtran235@gmail.com>
2020-10-21 21:12:12 -0700

    Fix websocket url

--- client/src/hoc/withWebsocket.js
+++ client/src/hoc/withWebsocket.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 const withWebsocket = (ComponentToWrap) => {
   return class WebSocketComponent extends Component {
     render() {
-      const socket = io('http://localhost:5000');
+      const socket = io('https://lets-socialize.herokuapp.com');
       return <ComponentToWrap socket={socket} />;
     }
   };
